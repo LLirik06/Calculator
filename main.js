bea5a68db5802c27b64789b17769b447c3e30298
Kirill <dremkiri@gmail.com>
2022-01-16 17:22:15 +0200

    1

---
+++ main.js
@@ -0,0 +1,10 @@
+let a = +prompt("Введите первое число")
+let b = +prompt("Введите второе число")
+summa = `Сумма этих чисел, ${a+b}`
+minus = `Разность этих чисел, ${a-b}`
+umnoj = `Произведение этих чисел, ${a*b}`
+delenie = `Частное этих чисел ${a/b}`
+alert(summa)
+alert(minus)
+alert(umnoj)
+alert(delenie)
